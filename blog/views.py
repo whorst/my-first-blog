643b6aa0b03a6a122c5ec98266bf4d5da43ce8c8
William Horst <whorst042@gmail.com>
2016-03-19 09:45:22 -0400

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+#This is used for declaring views "Similar to android"
+
 # Create your views here.
